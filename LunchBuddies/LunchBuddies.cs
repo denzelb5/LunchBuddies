68526e625f1dffbc1de1999c9094352726229e2d
denise baker <denviol@yahoo.com>
2020-03-14 11:13:58 -0500

    Add project files.

---
+++ LunchBuddies/LunchBuddies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class LunchBuddies
+    {
+        public string FirstName { get; set;  } 
+        public string LastName { get; set; }
+        public LunchBuddies(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
---
+++ LunchBuddies/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class Restaurant
+    {
+        public List<string> Restaurants = new List<string> { "Frothy Monkey", "Thai Esane", "Burger Up", "Mockingbird" };
+        public string Name { get; set; }
+
+        public Restaurant()
+        {
+            var Random = new Random();
+            int Index = Random.Next(Restaurants.Count);
+            Name = Restaurants[Index];
+        }
+    }
+}
