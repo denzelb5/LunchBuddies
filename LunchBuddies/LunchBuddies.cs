73d127b51ca3ccd07b320b5b9fea23d03cf95c13
denise baker <denviol@yahoo.com>
2020-03-14 20:10:32 -0500

    finished Lunchbuddies

--- LunchBuddies/LunchBuddies.cs
+++ LunchBuddies/LunchBuddies.cs
@@ -2,16 +2,51 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace LunchBuddies
 {
-    class LunchBuddies
+    class LunchBuddy
     {
+        
         public string FirstName { get; set;  } 
         public string LastName { get; set; }
-        public LunchBuddies(string firstName, string lastName)
+        public LunchBuddy(string firstName, string lastName)
         {
-            this.FirstName = firstName;
-            this.LastName = lastName;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string Eat()
+        {
+            var restaurant = new Restaurant();
+            Console.WriteLine($@"{FirstName} {LastName} is having lunch at {restaurant.Name}");
+            return restaurant.Name;
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($@"{FirstName} {LastName} ate {food} at the office today.");
+        }
+
+        public void Eat(List<LunchBuddy> companions)
+        {
+            var restaurant = new Restaurant();
+            Console.WriteLine($@"{FirstName} {LastName} is eating at {restaurant.Name} with his lunchbuddies");
+
+            foreach (var lunchBud in companions)
+            {
+                Console.WriteLine($"{lunchBud.FirstName}");
+            }
+        }
+
+        public void Eat(string food, List<LunchBuddy> companions)
+        {
+            var restaurant = new Restaurant();
+                Console.WriteLine($@"{FirstName} {LastName} is eating {food} at {restaurant.Name} with his lunchbuddies");
+            foreach (var lunchBud in companions)
+            {
+                Console.WriteLine($"{lunchBud.FirstName}");
+            }
         }
     }
 }
--- LunchBuddies/Program.cs
+++ LunchBuddies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LunchBuddies
 {
@@ -7,6 +8,26 @@ namespace LunchBuddies
         static void Main(string[] args)
         {
             
+
+            List<LunchBuddy> companions = new List<LunchBuddy> { };
+
+            var lunchBud1 = new LunchBuddy("Joe", "Schmoe");
+            var lunchBud2 = new LunchBuddy("Laura", "Downing");
+            var lunchBud3 = new LunchBuddy("Alice", "Smith");
+            var lunchBud4 = new LunchBuddy("Stevie", "Jones");
+
+            companions.Add(lunchBud1);
+            companions.Add(lunchBud2);
+            companions.Add(lunchBud3);
+            companions.Add(lunchBud4);
+
+            var LunchBuddy1 = new LunchBuddy("Mike", "Smith");
+            LunchBuddy1.Eat();
+            LunchBuddy1.Eat("tacos");
+            lunchBud4.Eat(companions);
+            LunchBuddy1.Eat("burritos", companions);
+
+           
         }
     }
 }
